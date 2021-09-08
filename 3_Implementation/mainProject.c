9f6bd0667753c23fdd06046aa7edf05dbbe481bd
Shaik Arshad <shaikarshadjanbee@gmail.com>
2021-09-08 22:13:44 +0530

    removed windows .h

--- 3_Implementation/mainProject.c
+++ 3_Implementation/mainProject.c
@@ -36,41 +36,41 @@ int main(){
      * @brief setxy is function it takes the x,y int input and set their position on the compiler
      *
      */
-    setxy(45,0);
+   
     printf("--------------------------------------");
-    setxy(48,1);
+    
     printf("Hi! There Please Enter the Details");
-    setxy(45,2);
+  
     printf("--------------------------------------");
-    setxy(35,5);
+   
     printf("*******************************************************\n");
     
     for(int pos=5;pos<12;pos++){
-    setxy(35,pos);
+   
     printf("*");
     }
     for(int pos=5;pos<12;pos++){
-    setxy(90,pos);
+    
     printf("*");
     }
 
-   setxy(40,7);
+  
    printf("First Name\t:");
-   setxy(40,8);
+  
    printf("Last Name\t:");
-   setxy(40,9);
+  
    printf("Email id\t:");
-   setxy(40,10);
+  
    printf("Phone Number\t:");
-   setxy(35,12);
+  
    printf("*******************************************************\n");
-   setxy(60,7);
+   
    scanf("%s",U.first);
-   setxy(60,8);
+ 
    scanf("%s",U.last);
-   setxy(60,9);
+   
    scanf("%s",U.email);
-   setxy(60,10);
+  
    scanf("%s",U.phone);
 
 
