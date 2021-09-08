ad1a1816fc4fa9d84907f0609dcd7c30931c22bb
Shaik Arshad <shaikarshadjanbee@gmail.com>
2021-09-08 17:27:02 +0530

    updated test

--- 3_Implementation/test/test_ebook.c
+++ 3_Implementation/test/test_ebook.c
@@ -4,3 +4,23 @@
 #include "user.h"
 #include "topics.h"
 
+/* Prototypes for all the test functions */
+void test_setxy(void);
+void test_subtract(void);
+void test_multiply(void);
+void test_divide(void);
+void test_power(void);
+void test_squareroot(void);
+void test_logarithm(void);
+void test_factorial(void);
+void test_sine(void);
+void test_cosine(void);
+void test_tangent(void);
+void test_cotangent(void);
+void test_secant(void);
+void test_cosecant(void);
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
